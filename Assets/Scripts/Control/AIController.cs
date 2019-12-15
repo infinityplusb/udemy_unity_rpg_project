6a588ca88464b13e91e57cd22b3d84a748ad7862
brian <brian@infinityplusb.com>
2019-12-15 21:59:05 +1100

    Added in AI to chase Player and Damage

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RPG.Combat;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        Fighter fighter;
+        GameObject player;
+        // Start is called before the first frame update
+        void Start()
+        {
+            fighter = GetComponent<Fighter>();
+            player = GameObject.FindWithTag("Player");
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+            if(InAttackRange() && fighter.CanAttack(player))
+            {
+                print(gameObject.name + " Chase!");
+                fighter.Attack(player);
+            }
+            else
+            {
+                fighter.Cancel();
+            }
+        }
+
+        private bool InAttackRange()
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            return distanceToPlayer < chaseDistance ;
+        }
+    }
+}
