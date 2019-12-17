051215d64b756255ccf0bb804b528c4e9a7c1c7c
brian <brian@infinityplusb.com>
2019-12-18 08:26:27 +1100

    Added Waypoints

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -12,7 +12,8 @@ namespace RPG.Control
     {
         [SerializeField] float chaseDistance = 5f;
         [SerializeField] float suspicionTime = 3f;
-
+        [SerializeField] PatrolPath patrolPath ;
+        [SerializeField] float waypointTolerance = 1f;
 
         Fighter fighter;
         GameObject player;
@@ -28,7 +29,11 @@ namespace RPG.Control
             player = GameObject.FindWithTag("Player");
             mover = GetComponent<Mover>();
 
-            guardPosition = transform.position;
+            if(patrolPath != null)
+              guardPosition = patrolPath.GetChild(0).position;
+            else
+              guardPosition = transform.position;
+
         }
 
         // Update is called once per frame
@@ -48,15 +53,43 @@ namespace RPG.Control
                 }
                 else
                 {
-                    GuardBehaviour();
+                    PatrolBehaviour();
                 }
             }
             timeSinceLastSawPlayer += Time.deltaTime;
         }
 
-        private void GuardBehaviour()
+        private void PatrolBehaviour()
         {
-            mover.StartMoveAction(guardPosition);
+            Vector3 nextPostion = guardPosition;
+
+            if(patrolPath != null)
+            {
+                if (AtWaypoint())
+                {
+                    CycleWaypoint();
+                }
+                nextPosition = GetCurrentWaypoint();
+            }
+            mover.StartMoveAction(nextPosition);
+        }
+
+        private bool AtWaypoint()
+        {
+            if(Vector3.Distance(transform.position, nextPosition) <= waypointTolerance)
+              return true;
+        }
+
+        private bool CycleWaypoint()
+        {
+            GetComponent<PatrolPath>().GetNextIndex()
+        }
+
+        private int GetCurrentWaypoint()
+        {
+            for(int i = 0; i < transform.childCount; i++ )
+              if(transform.GetChild(i).position == transform.position)
+                return i;
         }
 
         private void SuspicionBehaviour()
--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PatrolPath : MonoBehaviour
+namespace RPG.Control
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PatrolPath : MonoBehaviour
     {
-        
-    }
+      const float waypointGizmoRadius = 0.3f;
+      // Start is called before the first frame update
+      void Start()
+      {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+
+      }
+
+      private void OnDrawGizmos()
+      {
+          for(int i = 0; i < transform.childCount; i++ )
+          {
+              int j = GetNextIndex(i);
+
+              Gizmos.color = Color.blue;
+              Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+              Gizmos.color = Color.white;
+              Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+          }
+      }
+
+      private Vector3 GetWaypoint(int i)
+      {
+          return transform.GetChild(i).position;
+      }
+
+      private int GetNextIndex(int i)
+      {
+          if(i < transform.childCount - 1)
+            return i+1;
+          else
+            return 0;
+      }
+
+
     }
 }
